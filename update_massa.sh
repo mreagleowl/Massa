b0235f6e5ed65531b8660a8e528d7f2fb4388857
mreagleowl <90679212+mreagleowl@users.noreply.github.com>
2023-06-08 08:59:34 +0300

    Update update_massa.sh

--- update_massa.sh
+++ update_massa.sh
@@ -28,12 +28,12 @@ sleep 2
 echo ''
 echo '------------ Downloading new Massa -----------'
 echo ''
-wget https://github.com/massalabs/massa/releases/download/TEST.22.2/massa_TEST.22.2_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.23.0/massa_TEST.23.0_release_linux.tar.gz
 sleep 2
 echo ''
 echo '------------ Unzipping new Massa -------------'
 echo ''
-tar zxvf massa_TEST.22.2_release_linux.tar.gz
+tar zxvf massa_TEST.23.0_release_linux.tar.gz
 sleep 2
 rm /root/massa_TEST.*.tar.gz
 sleep 2 
